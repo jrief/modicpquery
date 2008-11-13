2013962d612c599dc00342d4813f486f9c789992
jacob.rief <jacob.rief@677f0394-a5fc-11dd-9bad-6bc74a82fd44>
2008-11-13 22:01:10 +0000

    version 2.0.2

---
+++ tags/mod_icpquery-2.0.2/apr_ext.c
@@ -0,0 +1,29 @@
+
+#define APR_WANT_MEMFUNC
+#include <apr_want.h>
+#include <apr_tables.h>
+
+APR_DECLARE(apr_array_header_t *) 
+apr_array_shuffle_ext(apr_pool_t * pool,  const apr_array_header_t * sorted)
+{
+	apr_array_header_t* shuffled = apr_array_make(pool, sorted->nelts, sorted->elt_size);
+	int nelts = shuffled->nelts = sorted->nelts;
+	int elt_size = sorted->elt_size;
+	int bufsize = elt_size * nelts;
+	char buffer[bufsize];
+	memcpy(buffer, sorted->elts, bufsize);
+	char * shelts = shuffled->elts;
+	unsigned int rnd;
+	int i;
+	for (i = 0; i<sorted->nelts; ++i) {
+		apr_generate_random_bytes((unsigned char*)&rnd, sizeof(rnd));
+		rnd %= nelts;
+		memcpy(shelts, buffer + rnd*elt_size, elt_size);
+		if (nelts-rnd>1)
+			memmove(buffer+rnd*elt_size, buffer+(rnd+1)*elt_size, (nelts-rnd-1)*elt_size);
+		--nelts;
+		shelts += elt_size;
+	}
+	return shuffled;
+}
+
