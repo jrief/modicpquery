32c89601a7fad1b7f7d6b89ccccc264084bec0f2
jacob.rief <jacob.rief@677f0394-a5fc-11dd-9bad-6bc74a82fd44>
2008-12-22 22:02:14 +0000

    optimized apr_array_shuffle

--- mod_icpquery/apr_ext.c
+++ mod_icpquery/apr_ext.c
@@ -8,25 +8,24 @@ apr_array_shuffle_ext(apr_pool_t * pool, apr_array_header_t * sorted)
 {
     apr_array_header_t* shuffled = apr_array_make(pool, sorted->nelts, sorted->elt_size);
     if (shuffled==NULL) {
-        // is it better to return an unshuffled array, than returning nothing?
+	// in case malloc failed, is it better to return an unshuffled array, rather than null?
         return sorted;
     }
-    int nelts = shuffled->nelts = sorted->nelts;
+    shuffled->nelts = sorted->nelts;
     int elt_size = sorted->elt_size;
-    int bufsize = elt_size * nelts;
-    char buffer[bufsize];
-    memcpy(buffer, sorted->elts, bufsize);
-    char * shelts = shuffled->elts;
-    unsigned int rnd;
-    int i;
-    for (i = 0; i<sorted->nelts; ++i) {
+    char * base = shuffled->elts;
+    memcpy(base, sorted->elts, elt_size * sorted->nelts);
+    int nelts;
+    for (nelts = sorted->nelts; nelts>0; --nelts) {
+        unsigned int rnd;
         apr_generate_random_bytes((unsigned char*)&rnd, sizeof(rnd));
         rnd %= nelts;
-        memcpy(shelts, buffer + rnd*elt_size, elt_size);
-        if (nelts-rnd>1)
-            memmove(buffer+rnd*elt_size, buffer+(rnd+1)*elt_size, (nelts-rnd-1)*elt_size);
-        --nelts;
-        shelts += elt_size;
+        char * randpos = base + rnd*elt_size;
+        char swapbuf[elt_size];
+        memcpy(swapbuf, base, elt_size);
+        memcpy(base, randpos, elt_size);
+        memcpy(randpos, swapbuf, elt_size);
+        base += elt_size;
     }
     return shuffled;
 }
